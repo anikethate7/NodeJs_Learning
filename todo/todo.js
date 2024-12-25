6084ec8cf0bad8aff290f0756f0a02ee88ba500d
Aniket Hate <76146742+anikethate7@users.noreply.github.com>
2024-12-25 14:10:26 +0530

    add, load and save task

--- todo/todo.js
+++ todo/todo.js
@@ -1,6 +1,30 @@
 const fs = require('fs') ;
 const filePath  = "./tasks.json" ;
 
+const loadTasks = () => {
+    try {
+        dataBuffer = fs.readFileSync(filePath);
+        dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON)
+    } catch (error) {
+        return [];
+    }
+}
+
+const saveTasks = (tasks) => {
+    const dataJSON = JSON.stringify(tasks)
+    fs.writeFileSync(filePath, dataJSON)
+    console.log("Task added", task)
+}
+const addTask = (task) =>{
+    const tasks = loadTasks();
+    tasks.push(task);
+    saveTasks(tasks);
+}
+
+const command = process.argv[2] ;
+const argument = process.argv[3] ;
+
 if(command === "add"){
     addTask(argument);
 }else if(command === "list"){
